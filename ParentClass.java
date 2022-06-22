751b12acda68041dbb1b50f07af94e721a8370ef
Jenita2022 <105583253+Jenita2022@users.noreply.github.com>
2022-06-22 16:51:20 +0530

    Add files via upload

--- ParentClass.java
+++ ParentClass.java
@@ -1,30 +1,47 @@
-package week6.day1;
+package week6.day2;
 
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import org.testng.annotations.BeforeMethod;
-
+import java.io.IOException;
 import java.time.Duration;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class ParentClass {
 	
 public ChromeDriver driver;
+public String fileName;
 
-  @BeforeMethod
-  public void precondition() {
-	  	WebDriverManager.chromedriver().setup();
+	@Parameters({"url","username","password"})
+	
+	@BeforeMethod
+	public void precondition(String url, String uName, String pwd) {
+		WebDriverManager.chromedriver().setup();
 		driver = new ChromeDriver();
-		driver.get("http://www.leafground.com/");
+		driver.get(url);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
-  }
+		
+		driver.findElement(By.id("username")).sendKeys(uName);
+		driver.findElement(By.id("password")).sendKeys(pwd);
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+	}
 
-  @AfterMethod
-  public void postcondition() {
-	  driver.close();
-  }
-
+	@AfterMethod
+	public void postcondition() {
+	driver.close();
+	}
+	
+	@DataProvider
+	public String[][] fetchData() throws IOException {
+	String[][] readdata = ReadExcel_New.readData(fileName);
+	return readdata;
+	
+}
 }
